749fc4ebfd87688fe3a381bf82a75c2b8b1c3e00
Anish Parepalli <aparep7474@Anishs-MacBook-Pro.local>
2021-06-21 09:38:17 -0400

    Added more enchants and fixed registration, should appear in game now

---
+++ src/main/kotlin/com/prince/skyblocksandbox/skyblockenchants/EnchantGK.kt
@@ -0,0 +1,33 @@
+package com.prince.skyblocksandbox.skyblockenchants
+
+import org.bukkit.enchantments.EnchantmentTarget
+import com.prince.skyblocksandbox.skyblockenchants.SBEnchants
+import org.bukkit.Material
+import org.bukkit.enchantments.Enchantment
+import org.bukkit.inventory.ItemStack
+
+class EnchantGK : Enchantment(101) {
+    override fun getName(): String {
+        return "Giant Killer"
+    }
+
+    override fun getMaxLevel(): Int {
+        return 7
+    }
+
+    override fun getStartLevel(): Int {
+        return 1
+    }
+
+    override fun getItemTarget(): EnchantmentTarget {
+        return EnchantmentTarget.WEAPON
+    }
+
+    override fun conflictsWith(other: Enchantment): Boolean {
+        return other === SBEnchants.titanKiller
+    }
+
+    override fun canEnchantItem(item: ItemStack): Boolean {
+        return item.type == Material.DIAMOND_SWORD
+    }
+}
--- src/main/kotlin/com/prince/skyblocksandbox/skyblockenchants/SBEnchants.kt
+++ src/main/kotlin/com/prince/skyblocksandbox/skyblockenchants/SBEnchants.kt
@@ -1,8 +1,7 @@
 package com.prince.skyblocksandbox.skyblockenchants
 
-import com.prince.skyblocksandbox.skyblockenchants.CustomEnchantment
-import com.prince.skyblocksandbox.skyblockenchants.EnchantGiantKiller
+import com.prince.skyblocksandbox.skyblockenchants.EnchantGK
 
 object SBEnchants {
-    val GIANT_KILLER: CustomEnchantment = EnchantGiantKiller()
+    val giantKiller = EnchantGK()
 }
