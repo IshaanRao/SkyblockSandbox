72a5e03a5e1296a51bf813952ba56ac2af070ae5
Ishaan Rao <ishaanrao26@gmail.com>
2021-07-08 21:43:09 -0700

    Fixed gk lore

--- src/main/kotlin/com/prince/skyblocksandbox/skyblockenchants/EnchantGiantKiller.kt
+++ src/main/kotlin/com/prince/skyblocksandbox/skyblockenchants/EnchantGiantKiller.kt
@@ -10,7 +10,7 @@ object EnchantGiantKiller : SkyblockEnchant {
     override val levelRange: IntRange = 1..7
     override val name: String = "Giant Killer"
     override fun descAtLevel(level: Int): List<String> {
-        return listOf("§7Increases damage dealt by §a${0.1*level}%","§7for each percent of extra","§7health that your target has","§7above you up to §a${5*level}%")
+        return listOf("§7Increases damage dealt by §a0.${1*level}%","§7for each percent of extra","§7health that your target has","§7above you up to §a${5*level}%")
     }
     override val items: ItemTypes = ItemTypes.SWORD
     override fun getAddedDamage(mob: SkyblockMob, player: Player, level:Int): Double {
